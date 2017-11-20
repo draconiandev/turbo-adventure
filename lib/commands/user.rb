871167e9434dec7ae7d7066379d02dd41001f2d6
p150 <pavanprakash21@gmail.com>
2017-11-21 03:11:06 +0530

    Integrate Create User feature

--- lib/commands/user.rb
+++ lib/commands/user.rb
@@ -6,13 +6,16 @@ module Commands
     #                    username]
     # returns account_number if user is added
     # raises ArgumentError if user is not added due to bad arguments
-    def create(args)
+    def self.create(args)
       # name, phone, address, email, pan, aadhar, passport, username = args
       # Models::User.create(name: name, phone: phone, address: address,
       #                     email: email, pan: pan, aadhar: aadhar,
       #                     passport: passport, username: username)
-      name = args
-      Models::User.new.print(name.first)
+      args_hash = { name: args[0], phone: args[1], address: args[2],
+                    email: args[3], pan: args[4], aadhar: args[5],
+                    passport: args[6], username: args[7]
+    }
+      Models::User.create_from_cli(args_hash)
     end
 
     def list
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -4,8 +4,31 @@ module Models
   # Defines the User class. Also provides a wrapper for add,
   # list and remove methods
   class User < ActiveRecord::Base
-    def print(name)
-      p 'printed: ' + name
+    before_create :generate_account_number
+
+    def self.create_from_cli(args)
+      # create!(args)
+      user = new(args)
+      if user.save
+        puts 'Account Number for the created account: ' + user.account_number
+      else
+        p user.errors
+      end
+    end
+
+    private
+
+    def generate_account_number
+      return if account_number.present?
+      self.account_number = generate_account_token
+    end
+
+    def generate_account_token
+      require 'securerandom'
+      loop do
+        token = SecureRandom.hex(3).upcase
+        break token unless User.find_by(account_number: token)
+      end
     end
   end
 end
