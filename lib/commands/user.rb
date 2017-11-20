ab2aede639726c60860a88277c2a63baa0ddff75
p150 <pavanprakash21@gmail.com>
2017-11-21 02:40:26 +0530

    Setup database and define schema for users table

--- lib/commands/user.rb
+++ lib/commands/user.rb
@@ -7,9 +7,7 @@ module Commands
     # returns account_number if user is added
     # raises ArgumentError if user is not added due to bad arguments
     def create(args)
-      puts 'asdasdasd'
       name, phone, address, email, pan, aadhar, passport, username = args
-      puts "Name: #{name}"
       Models::User.create(name: name, phone: phone, address: address,
                           email: email, pan: pan, aadhar: aadhar,
                           passport: passport, username: username)
---
+++ lib/config/schema.rb
@@ -0,0 +1,14 @@
+require 'active_record'
+
+ActiveRecord::Schema.define(version: 1) do
+  create_table :users, force: :cascade do |t|
+    t.string :name, default: '', null: false
+    t.string :phone, default: '', null: false
+    t.string :address, default: '', null: false
+    t.string :email, default: '', null: false
+    t.string :pan, default: '', null: false
+    t.string :aadhar, default: '', null: false
+    t.string :passport, default: '', null: false
+    t.string :account_number, default: '', null: false
+  end
+end
