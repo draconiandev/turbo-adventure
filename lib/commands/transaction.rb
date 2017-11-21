e3e43ec2d9945c68ef845fa0060b77342dd029a2
p150 <pavanprakash21@gmail.com>
2017-11-21 20:21:48 +0530

    Add lend sub command to transact

--- lib/commands/transaction.rb
+++ lib/commands/transaction.rb
@@ -7,5 +7,12 @@ module Commands
       amount = args[2]
       Models::Transaction.transfer(from_account, to_account, amount)
     end
+
+    def self.lend(args)
+      to_account = args[0]
+      amount = args[1]
+      to = Models::User.find_by(account_number: to_account)
+      Models::User.add_balance(to, amount)
+    end
   end
 end
--- lib/models/user.rb
+++ lib/models/user.rb
@@ -9,6 +9,7 @@ module Models
     has_many :received_transactions, class_name: 'Transaction',
                                      foreign_key: :receiver_id
 
+    validates :balance, numericality: { greater_than_or_equal_to: 0 }
     before_create :generate_account_number
 
     def self.create_from_cli(args)
@@ -16,12 +17,38 @@ module Models
       if user.save
         puts 'Account Number for the created account: ' + user.account_number
       else
-        p user.errors
+        print_error_messages(user)
       end
     end
 
     def self.search(query)
       where('account_number OR name LIKE ?', "%#{query}%").order(id: :desc)
+    end
+
+    def self.add_balance(to, amount)
+      normalized_amount = Float(amount)
+      return invalid_amount unless normalized_amount.positive?
+      if to.update(balance: to.balance + normalized_amount)
+        puts amount + ' has been credited to ' + to.name
+      else
+        p to.errors
+      end
+    rescue ArgumentError
+      puts 'Invalid amount. Please use either an integer or a decimal.'
+    end
+
+    private_class_method
+
+    def self.print_error_messages(user)
+      puts 'Something went wrong with the transaction. Please review the errors'
+      user.errors.messages.each do |key, msg|
+        puts key.to_s + ' -> '
+        msg.each { |m| puts m.capitalize }
+      end
+    end
+
+    def self.invalid_amount
+      puts 'Amount must be positive'
     end
 
     private
