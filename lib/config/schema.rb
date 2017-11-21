b5fa0ae0c6aedbfcb93c538eca6853148a213b13
p150 <pavanprakash21@gmail.com>
2017-11-21 15:50:05 +0530

    Add validations to transfer feature

--- lib/config/schema.rb
+++ lib/config/schema.rb
@@ -3,23 +3,24 @@ require 'active_record'
 ActiveRecord::Schema.define(version: 1) do
   unless ActiveRecord::Base.connection.table_exists? :users
     create_table :users, force: :cascade do |t|
-      t.string :name,          default: '', null: false
-      t.string :phone,         default: '', null: false
-      t.string :address,       default: '', null: false
-      t.string :email,         default: '', null: false
-      t.string :pan,           default: '', null: false
-      t.string :aadhar,        default: '', null: false
-      t.string :passport
-      t.string :username
-      t.string :account_number, default: '', null: false
+      t.string  :name,          default: '', null: false
+      t.string  :phone,         default: '', null: false
+      t.string  :address,       default: '', null: false
+      t.string  :email,         default: '', null: false
+      t.string  :pan,           default: '', null: false
+      t.string  :aadhar,        default: '', null: false
+      t.string  :passport
+      t.string  :username
+      t.string  :account_number, default: '', null: false
+      t.decimal :balance,        default: 0,  null: false
     end
   end
 
   unless ActiveRecord::Base.connection.table_exists? :transactions
     create_table :transactions, force: :cascade do |t|
-      t.references  :sender,   default: '', null: false
-      t.references  :receiver, default: '', null: false
-      t.decimal     :amount,   default: 0,  null: false
+      t.references :sender,   default: '', null: false
+      t.references :receiver, default: '', null: false
+      t.decimal    :amount,   default: 0,  null: false
     end
   end
 end
--- lib/models/transaction.rb
+++ lib/models/transaction.rb
@@ -6,6 +6,11 @@ module Models
     belongs_to :receiver, class_name: 'User', foreign_key: :receiver_id
     belongs_to :sender, class_name: 'User', foreign_key: :sender_id
 
+    validate :can_transfer?
+    validates :amount, numericality: { greater_than: 0 }
+
+    after_create :calculate_balance
+
     def self.transfer(from, to, amount)
       from_user = Models::User.find_by(account_number: from)
       to_user = Models::User.find_by(account_number: to)
@@ -16,6 +21,8 @@ module Models
       handle_save(transact, amount, from_user, to_user)
     end
 
+    private_class_method
+
     def self.not_valid_accounts
       puts 'Not valid accounts'
     end
@@ -25,9 +32,28 @@ module Models
         puts amount + ' has been transferred from ' + from_user.name + ' to ' +
              to_user.name
       else
-        puts 'Something went wrong with the transaction. Please review the errors'
-        puts 'Errors: ' + transact.errors
+        print_error_messages(transact)
       end
+    end
+
+    def self.print_error_messages(transact)
+      puts 'Something went wrong with the transaction. Please review the errors'
+      transact.errors.messages.each do |key, msg|
+        puts key.to_s + ' -> '
+        msg.each { |m| puts m.capitalize }
+      end
+    end
+
+    private
+
+    def can_transfer?
+      return if sender.balance >= amount
+      errors.add(:amount, 'sender does not have enough balance to transfer this amount')
+    end
+
+    def calculate_balance
+      sender.update(balance: sender.balance - amount)
+      receiver.update(balance: receiver.balance + amount)
     end
   end
 end
